7a3c5d60a57944c702d5e464732a3bb308c3dfae
sunghwan Kim <iamsung33@neighbor21.co.kr>
2021-04-23 17:57:48 +0900

    Initial commit

---
+++ src/main/java/com/example/jpaminishop/JpaMiniShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaminishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMiniShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaMiniShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpaminishop/JpaMiniShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaminishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaMiniShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
